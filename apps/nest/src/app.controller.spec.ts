e05e0d9e93acd12f97d4f321e183a1fde788c256
spuxx1701 <accounts@spuxx.dev>
2024-08-10 21:07:30 +0200

    test: Fixed test

--- apps/nest/src/app.controller.spec.ts
+++ apps/nest/src/app.controller.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NestUtilsModule } from '@spuxx/nest-utils';
 
 describe('AppController', () => {
   let controller: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [NestUtilsModule],
       controllers: [AppController],
       providers: [AppService],
     }).compile();

951ce212375c372f91603326f547d809e04d829c
spuxx1701 <accounts@spuxx.dev>
2024-08-14 15:53:50 +0200

    chore: Remove VitePluginNode from vite config

--- packages/nest-utils/vite.config.ts
+++ packages/nest-utils/vite.config.ts
@@ -2,21 +2,11 @@
 import baseConfig from '../../vite.config';
 import { mergeConfig, defineConfig } from 'vite';
 import { resolve } from 'path';
-import { VitePluginNode } from 'vite-plugin-node';
 import { peerDependencies } from './package.json';
 
 export default mergeConfig(
   baseConfig,
   defineConfig({
-    plugins: [
-      ...baseConfig.plugins!,
-      VitePluginNode({
-        adapter: 'nest',
-        tsCompiler: 'swc',
-        appPath: 'src/main.ts',
-        exportName: 'app',
-      }),
-    ],
     build: {
       lib: {
         entry: {

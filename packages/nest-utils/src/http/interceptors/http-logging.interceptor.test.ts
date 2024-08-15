e7df43f718700fbc793f36d06e18d54e1a21a4c6
spuxx1701 <accounts@spuxx.dev>
2024-08-15 23:08:04 +0200

    feat: Implement HttpLoggingInterceptor

---
+++ packages/nest-utils/src/http/interceptors/http-logging.interceptor.test.ts
@@ -0,0 +1,41 @@
+import { Controller, Get, UseInterceptors } from '@nestjs/common';
+import { HttpLoggingInterceptor } from './http-logging.interceptor';
+import { Supertest, TestAppLogger, TestContainer } from '../../testing';
+import { ApplicationLogLevel } from '../../logging';
+
+describe('HttpLoggingInterceptor', () => {
+  let supertest: Supertest;
+  let logger: TestAppLogger;
+
+  @Controller()
+  class TestController {
+    @Get('hello')
+    @UseInterceptors(new HttpLoggingInterceptor())
+    hello() {
+      return 'Hello World!';
+    }
+  }
+
+  beforeEach(async () => {
+    logger = new TestAppLogger();
+    const container = await TestContainer.create({
+      controllers: [TestController],
+      logger,
+      enableEndToEnd: true,
+    });
+    supertest = container.supertest;
+    vitest.clearAllMocks();
+  });
+
+  it('should log both request and response', async () => {
+    logger.setLogLevel(ApplicationLogLevel.Verbose);
+    expect(logger.printMessages).toHaveBeenCalledTimes(0);
+    await supertest.get('/hello');
+    expect(logger.printMessages).toHaveBeenCalledTimes(2);
+  });
+
+  it('should log nothing when verbose logging is not enabled', async () => {
+    await supertest.get('/hello');
+    expect(logger.printMessages).toHaveBeenCalledTimes(0);
+  });
+});
---
+++ packages/nest-utils/src/http/interceptors/http-logging.interceptor.ts
@@ -0,0 +1,38 @@
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler, Logger } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+/**
+ * `HttpLoggingInterceptor` logs incoming HTTP requests.
+ * @example
+ * import { Controller, UseInterceptors } from '@nestjs/common';
+ * import { HttpLoggingInterceptor } from '@src/http/logging/logging.interceptor';
+ * // Apply globally...
+ * app.useGlobalInterceptors(new HttpLoggingInterceptor());
+ *
+ * // ...or to a controller...
+ * ＠Controller()
+ * ＠UseInterceptors(HttpLoggingInterceptor)
+ * export class MyController {}
+ *
+ * // ...or to a single entrypoint
+ * export class MyController {
+ *   ＠Get('cats')
+ *   ＠UseInterceptors(HttpLoggingInterceptor)
+ *   getCats() {}
+ * }
+ */
+@Injectable()
+export class HttpLoggingInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<object> {
+    const request = context.getArgByIndex(0);
+    const dateIn = new Date();
+    Logger.verbose(`Incoming request: ` + `${String(request.method).toUpperCase()} ${request.url}.`);
+    return next.handle().pipe(
+      tap(() => {
+        const dateOut = new Date();
+        Logger.verbose(`Request fulfilled after ${dateOut.getTime() - dateIn.getTime()}ms.`);
+      }),
+    );
+  }
+}

42b6a5b170074931868968cd3fdfcb2e7146666e
spuxx1701 <accounts@spuxx.dev>
2024-08-16 16:00:16 +0200

    chore: No longer lint typescript's naming conventions

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,33 +14,5 @@ module.exports = {
     // Miscellanous
     'no-console': ['error'],
     'no-debugger': ['error'],
-    /**
-     * Naming convention for variables, functions, methods, classes, interfaces,
-     */
-    '@typescript-eslint/naming-convention': [
-      'error',
-      {
-        selector: 'import',
-        format: ['camelCase', 'PascalCase'],
-      },
-      {
-        selector: 'default',
-        format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
-        leadingUnderscore: 'allow',
-      },
-      { selector: 'property', format: ['camelCase', 'PascalCase', 'UPPER_CASE'], leadingUnderscore: 'allow' },
-      { selector: 'property', modifiers: ['requiresQuotes'], format: null },
-      { selector: 'method', format: ['camelCase'], leadingUnderscore: 'allow' },
-      { selector: 'function', format: ['camelCase', 'PascalCase'] },
-      { selector: ['typeLike', 'enumMember'], format: ['PascalCase'] },
-      {
-        selector: 'objectLiteralProperty',
-        format: null,
-        filter: {
-          regex: '^(__html|_html)$',
-          match: true,
-        },
-      },
-    ],
   },
 };

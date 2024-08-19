13fd2fc074a2d1e9192e4731e6c79949f75fa510
spuxx1701 <accounts@spuxx.dev>
2024-08-19 15:45:43 +0200

    feat: Add `OmitFunctionMembers` helper type

--- packages/js-utils/src/types/public/helper-types.ts
+++ packages/js-utils/src/types/public/helper-types.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-types */
 /**
  * A type that extracts the optional properties from another type.
  */
@@ -10,3 +11,10 @@ export type OptionalProperties<T> = {
  */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type Constructor<T = any> = new (...args: any[]) => T;
+
+/**
+ * Creates a subsidiary type that omits all function members from a given type.
+ */
+export type OmitFunctionMembers<T> = {
+  [K in keyof T as T[K] extends Function ? never : K]: T[K];
+};

0578758c4c883c648faec12c108cacc1e9862064
spuxx1701 <accounts@spuxx.dev>
2024-09-01 18:38:54 +0200

    docs(env): Improved documentation of EnvModuleMixin

--- apps/nest/src/env/env.module.ts
+++ apps/nest/src/env/env.module.ts
@@ -1,4 +1,21 @@
-import { EnvModuleMixin } from '@spuxx/nest-utils';
-import { Environment } from './env.definition';
+import { EnvModuleMixin, ApplicationLogLevel } from '@spuxx/nest-utils';
+import { IsIn, IsString, IsUrl } from 'class-validator';
 
-export class EnvModule extends EnvModuleMixin<Environment>(Environment) {}
+class Env {
+  @IsString()
+  @IsIn(Object.values(ApplicationLogLevel))
+  APP_LOG_LEVEL: ApplicationLogLevel = ApplicationLogLevel.Default;
+
+  @IsString()
+  APP_BASE_URL: string = 'http://localhost:3000';
+
+  @IsUrl()
+  AUTH_ISSUER_URL: string = 'https://auth.spuxx.dev/realms/spuxx/';
+
+  @IsString()
+  AUTH_CLIENT_ID: string = 'test';
+
+  @IsString()
+  AUTH_CLIENT_SECRET: string;
+}
+export class EnvModule extends EnvModuleMixin<Env>(Env) {}
--- packages/nest-utils/src/env/env.module-mixin.ts
+++ packages/nest-utils/src/env/env.module-mixin.ts
@@ -3,6 +3,34 @@ import { plainToInstance } from 'class-transformer';
 import { validateSync } from 'class-validator';
 import { config } from 'dotenv';
 
+/**
+ * A mixin for a NestJS module that provides easy access to environment variables.
+ * The module also handles validation of the environment variables using `class-validator`
+ * on start-up.
+ * @param env The class that defines the environment variables.
+ * @returns The `EnvModule` class.
+ * @example
+ * // env.module.ts
+ * import { IsNumber, IsString } from 'class-validator';
+ * class Env {
+ *   ＠IsNumber()
+ *   PORT: number; // Will throw an error if not provided in .env file
+ *
+ *   ＠IsString()
+ *   GREETING_ROUTE: string = '/hello'; // Will default to '/hello' if not provided in .env file
+ * }
+ * export class EnvModule extends EnvModuleMixin<Env>(Env) {}
+ *
+ * // app.module.ts
+ * ＠Module({
+ *   imports: [EnvModule],
+ * })
+ * export class AppModule {}
+ *
+ * // You can access any environment variable through EnvModule.get()
+ * console.log(EnvModule.get('PORT')); // This is type-safe!
+ * ＠Get(EnvModule.get('GREETING_ROUTE')) // Works with decorators!
+ */
 export function EnvModuleMixin<TEnv extends object>(env: new (...args: unknown[]) => TEnv) {
   @Module({})
   class EnvModule {

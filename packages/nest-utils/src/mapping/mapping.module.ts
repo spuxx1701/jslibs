720bcb5e7f080a89b1a54c42d92a7632f7c09af1
spuxx1701 <accounts@spuxx.dev>
2024-10-26 23:16:16 +0200

    feat(mapping): MappingModule now supports sequelize

--- packages/nest-utils/src/mapping/mapping.module.ts
+++ packages/nest-utils/src/mapping/mapping.module.ts
@@ -1,11 +1,26 @@
-import { classes } from '@automapper/classes';
+import { Logger, Module } from '@nestjs/common';
 import { AutomapperModule } from '@automapper/nestjs';
-import { Module } from '@nestjs/common';
+import { classes } from '@automapper/classes';
 
 @Module({
   imports: [
-    AutomapperModule.forRoot({
-      strategyInitializer: classes(),
+    AutomapperModule.forRootAsync({
+      useFactory: async () => {
+        try {
+          const { sequelize } = await import('@automapper/sequelize');
+          return {
+            strategyInitializer: sequelize(),
+          };
+        } catch (error) {
+          Logger.verbose(
+            'Failed to import @automapper/sequelize. Falling back to @automapper/classes.',
+            MappingModule.name,
+          );
+          return {
+            strategyInitializer: classes(),
+          };
+        }
+      },
     }),
   ],
 })

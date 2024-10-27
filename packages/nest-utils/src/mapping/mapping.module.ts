afc545d37a5019ed8af643f359c600a8a26c8e44
spuxx1701 <accounts@spuxx.dev>
2024-10-27 23:12:06 +0100

    fix(mapping): Turned MappingModule into a global module

--- packages/nest-utils/src/mapping/mapping.module.ts
+++ packages/nest-utils/src/mapping/mapping.module.ts
@@ -1,6 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { Mapper } from './services/mapper';
 
+/**
+ * This module provides mapping capabilities through the `@Map()` decorator and  `Mapper` provider.
+ * As a global module, it should be imported once in the root module of your application.
+ */
+@Global()
 @Module({
   providers: [Mapper],
   exports: [Mapper],

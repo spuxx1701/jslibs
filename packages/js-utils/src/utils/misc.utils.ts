5ae1597542d50dfd2a27312f69a5cbe4052a30aa
spuxx1701 <accounts@spuxx.dev>
2024-11-03 21:08:18 +0100

    feat(utils): Add isEmptyArray function

--- packages/js-utils/src/utils/misc.utils.ts
+++ packages/js-utils/src/utils/misc.utils.ts
@@ -34,6 +34,15 @@ export function isEmptyOrWhitespace(value: unknown) {
   }
 }
 
+/**
+ * Returns true if the given array is null, undefined or empty.
+ * @param array The array to check.
+ * @returns Whether the given array is null, undefined or empty.
+ */
+export function isEmptyArray(array: Array<unknown> | undefined | null) {
+  return !array || array.length <= 0;
+}
+
 type RecursiveObject = {
   [key: string]: string | RecursiveObject;
 };

a11da65b63ea13078f55c2c169d46f26bb62bf40
spuxx1701 <accounts@spuxx.dev>
2024-11-06 11:45:14 +0100

    feat(transformers): Refactored `@TransformBooleanString` decorator and exposed `transformBooleanString()` function

--- packages/nest-utils/src/transformers/boolean-string.transformer.ts
+++ packages/nest-utils/src/transformers/boolean-string.transformer.ts
@@ -1,13 +1,20 @@
 import { Transform } from 'class-transformer';
 
 /**
- * Provides a decorator that transforms boolean strings to booleans.
- * Use via adding '@TransformBooleanString()' to a property.
- * @returns The transformer.
+ * Decorator that transforms boolean strings to booleans.
  * @example
  * ＠TransformBooleanString()
  * myBoolean: boolean; // 'true' and 'TRUE' becomes true, everything else becomes false
  */
 export function TransformBooleanString() {
-  return Transform(({ obj, key }) => obj[key].toString().toLowerCase() === 'true');
+  return Transform(transformBooleanString);
+}
+
+/**
+ * Transforms boolean strings to booleans.
+ * 'true' and 'TRUE' becomes true, everything else becomes false.
+ * @param value The value to transform.
+ */
+export function transformBooleanString({ value }: { value: unknown }): boolean {
+  return value.toString().toLowerCase() === 'true';
 }

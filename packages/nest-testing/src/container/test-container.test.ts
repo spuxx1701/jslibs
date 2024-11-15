a36c8241300648e3af7a1d8c8c4dcdaaff88a6a8
@LeopoldHock <leopold.hock@deutschebahn.com>
2024-11-15 15:02:12 +0100

    test(container): Improve testing of `TestContainer`

--- packages/nest-testing/src/container/test-container.test.ts
+++ packages/nest-testing/src/container/test-container.test.ts
@@ -1,17 +1,23 @@
-import { Controller, Injectable, Module } from '@nestjs/common';
+import { Controller, Get, Injectable, Module } from '@nestjs/common';
 import { TestContainer } from './test-container';
+import { AuthService } from 'packages/nest-utils/dist/main';
 
 describe('TestContainer', () => {
+  @Controller()
+  class TestController {
+    @Get('hi')
+    sayHi() {
+      return { message: 'Hi mom!' };
+    }
+  }
+
+  @Injectable()
+  class TestService {}
+
+  @Module({})
+  class TestModule {}
+
   it('should create a test container with the given dependencies', async () => {
-    @Controller()
-    class TestController {}
-
-    @Injectable()
-    class TestService {}
-
-    @Module({})
-    class TestModule {}
-
     const container = await TestContainer.create({
       controllers: [TestController],
       providers: [TestService],
@@ -24,6 +30,23 @@ describe('TestContainer', () => {
     expect(module.get(TestController)).toBeInstanceOf(TestController);
     expect(module.get(TestService)).toBeInstanceOf(TestService);
     expect(module.get(TestModule)).toBeInstanceOf(TestModule);
+  });
+
+  it('should create a container that supports end-to-end testing', async () => {
+    const container = await TestContainer.create({
+      controllers: [TestController],
+      providers: [TestService],
+      imports: [TestModule],
+      enableEndToEnd: true,
+    });
+    const { app, supertest } = container;
+
+    expect(app).toBeDefined();
+    expect(app.get(TestController)).toBeInstanceOf(TestController);
+    expect(app.get(TestService)).toBeInstanceOf(TestService);
+    expect(app.get(TestModule)).toBeInstanceOf(TestModule);
+    expect(app.get(AuthService)).toBeInstanceOf(AuthService);
+    expect((await supertest.get('/hi')).body).toEqual({ message: 'Hi mom!' });
   });
 
   it('should call the afterCreate hook', async () => {

33881a9c779c1006b07889cceca86080781928a9
spuxx1701 <accounts@spuxx.dev>
2025-02-16 23:16:19 +0100

    docs(storybook): Remove unused component

--- apps/storybook/src/components/markdown.component.tsx
+++
@@ -1,8 +0,0 @@
-import { Component } from 'solid-js';
-
-export const Markdown: Component<{ html?: string }> = (props) => {
-  const { html } = props;
-  return <div>Hello World!</div>;
-  // if (!html) return null;
-  // return <div innerHTML={props.html} />;
-};

80e2d6fcf761e512ec930431ffc3d855dc058bff
spuxx1701 <accounts@spuxx.dev>
2025-02-27 22:21:48 +0100

    test(container): Implement tests

---
+++ packages/solid/src/components/layout/container/container.component.test.tsx
@@ -0,0 +1,40 @@
+import { describe, it, expect } from 'vitest';
+import { render } from '@solidjs/testing-library';
+import { Container } from './container.component';
+import { BaseColor } from '@spuxx/browser-utils';
+
+describe('Container', () => {
+  it('should render with default values', async () => {
+    const { getByText } = render(() => <Container>Content</Container>);
+    const container = getByText('Content');
+    expect(container).toBeInTheDocument();
+    expect(container).toHaveClass('spx-container');
+    expect(container).toHaveTextContent('Content');
+    expect(container.tagName.toLowerCase()).toBe('div');
+    expect(container).not.toHaveAttribute('spx-variant');
+    expect(container).toHaveAttribute('spx-color', 'surface');
+    expect(container).not.toHaveAttribute('spx-no-padding');
+  });
+
+  it('should render with custom values', async () => {
+    const { getByText } = render(() => (
+      <Container
+        tag="article"
+        class="my-class"
+        color={BaseColor.gradient}
+        variant="contained"
+        noPadding
+      >
+        Content
+      </Container>
+    ));
+    const container = getByText('Content');
+    expect(container).toBeInTheDocument();
+    expect(container).toHaveClass('spx-container');
+    expect(container).toHaveTextContent('Content');
+    expect(container.tagName.toLowerCase()).toBe('article');
+    expect(container).toHaveAttribute('spx-variant', 'contained');
+    expect(container).toHaveAttribute('spx-color', 'gradient');
+    expect(container).toHaveAttribute('spx-no-padding');
+  });
+});
